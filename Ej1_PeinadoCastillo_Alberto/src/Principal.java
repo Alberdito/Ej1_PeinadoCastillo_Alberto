a78b8da6e18fbd42d0306cda8f57e5c7015f9b2b
usuario <usuario@DESKTOP-JBT006P>
2023-02-23 09:10:35 +0100

    Principal

---
+++ Ej1_PeinadoCastillo_Alberto/src/Principal.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner entrada = new Scanner(System.in);
+		
+		double dPeso, dAltura, dIMC;
+		String sDiagnostico;
+		
+		System.out.println("Introduzca su peso (Kg):");
+		dPeso = entrada.nextDouble();
+		
+		System.out.println("Ahora introduzca su altura (m)");
+		dAltura = entrada.nextDouble();
+		
+		dIMC = dPeso/(dAltura*dAltura);
+		
+		if(dIMC < 18.5)
+		{
+			sDiagnostico = "Bajo peso";
+		}
+		else if (dIMC <= 24.9)
+		{
+			sDiagnostico = "Peso normal (saludable)";
+		}
+		else if (dIMC <= 29.9)
+		{
+			sDiagnostico = "Sobrepeso";
+		}
+		else if (dIMC <= 40)
+		{
+			sDiagnostico = "Obesidad premórbida";
+		}
+		else
+		{
+			sDiagnostico = "Obesidad mórbida";
+		}
+		
+		System.out.printf("El paciente de %.2f kg y %.2f m de altura tiene un IMC de %.2f y tiene un diagnóstico de: %s", dPeso, dAltura, dIMC, sDiagnostico);
+	}
+
+}
